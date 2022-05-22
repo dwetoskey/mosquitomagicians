98d353b782d6052f6b3725237cfc36d3d76c976b
dwetoskey <dwetoskey224@gmail.com>
2022-05-22 11:17:57 -0400

    added flea fact card, added factcard height to app.css

--- src/components/Facts/index.js
+++ src/components/Facts/index.js
@@ -6,17 +6,18 @@ import { Container } from "react-bootstrap";
 import wnv from '../../images/wnv.png';
 import tick from '../../images/ticks.jpg';
 import { AiOutlineMail } from 'react-icons/ai';
+import flea from '../../images/flea.jpg';
 
 
 function Facts() {
 
     return (
         <Container>
-             <h1 className="border-bottom">Facts</h1>
-            <div className="d-flex flex-wrap align-items-center m-auto">
+             <h1 className="border-bottom">Common Pest Facts</h1>
+            <div className="d-flex flex-wrap">
            
             
-            <Card className="m-auto h-50" style={{ width: '18rem' }} >
+            <Card className="m-auto cardheight" style={{ width: '18rem' }} >
                 <Card.Img variant="top" src={wnv} />
                 <Card.Body>
                     <Card.Title>Mosquitos</Card.Title>
@@ -27,14 +28,14 @@ function Facts() {
                 <ListGroup className="list-group-flush">
                 </ListGroup>
                 <Card.Body>
-                    <Card.Link href="https://www.michigan.gov/emergingdiseases/mosquitoes" target="_blank">Michigan.Gov</Card.Link>
+                    <Card.Link href="https://www.michigan.gov/emergingdiseases/mosquitoes" target="_blank">Michigan.Gov Mosquito Info</Card.Link>
                 </Card.Body>
                 <Card.Body>
                     <h3>Call Us Now @<br /><a className="p-2" href="tel:+1-517-513-4554">517-513-4554</a></h3>
                 </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }} className="m-auto" >
+            <Card style={{ width: '18rem' }} className="m-auto cardheight" >
                 <Card.Img variant="top" src={tick} />
                 <Card.Body>
                     <Card.Title>Ticks</Card.Title>
@@ -47,31 +48,31 @@ function Facts() {
                 <Card.Body>
                     <Card.Link href="https://www.michigan.gov/mdhhs/-/media/Project/Websites/emergingdiseases/Folder3/Ticks_and_Your_Health_05_19.pdf" target="_blank">Michigan.Gov Tick Info</Card.Link>
                     <Card.Body>
-                    <h3>Email Us<br /><a className="p-2" href="mailto:robertwetoskey@gmail.com?subject=Quote&body=Please give us your name, address, and phone number and tell us what you would like treatment for."><AiOutlineMail size={40}/></a></h3>
+                    <h3>Email Us<br /><a className="p-2" href="mailto:robertwetoskey@gmail.com?subject=Quote&body=Please give us your name, address, and phone number and tell us what you would like treatment for. We will get back with you as soon as possible."><AiOutlineMail size={40}/></a></h3>
                 </Card.Body>
                     
                 </Card.Body>
             </Card>
+            
 
-            {/* <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={placeholderimg} />
+            <Card style={{ width: '18rem' }} className="m-auto cardheight" >
+                <Card.Img variant="top" src={flea} />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Fleas</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                        Fleaborne diseases include Typhus, tapeworms, Cat Scratch Disease, and even Plague. Call and ask us how we can help keep your pets safe from fleas this season.
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
                 </ListGroup>
                 <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
+                    <Card.Link href="https://www.cdc.gov/fleas/diseases.html" target="_blank">CDC Fleaborne Diseases Info</Card.Link>
+                    <Card.Body>
+                    <h3>Call Us Now @<br /><a className="p-2" href="tel:+1-517-513-4554">517-513-4554</a></h3>
                 </Card.Body>
-            </Card> */}
+                    
+                </Card.Body>
+            </Card>
             </div>
         </Container>
     )
